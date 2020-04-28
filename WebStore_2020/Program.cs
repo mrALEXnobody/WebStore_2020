b1a38341659eb027f1a4f98ee0a0bbc5e433f8fd
Александр Гусев <scorp.predateur@gmail.com>
2020-04-28 20:41:45 +0300

    Занятие 5: инициализация БД стартовыми данными

--- WebStore_2020/Program.cs
+++ WebStore_2020/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebStore.DAL;
 
 namespace WebStore_2020
 {
@@ -13,14 +16,40 @@ namespace WebStore_2020
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = BuildWebHost(args);
+
+            // нужно для получения DbContext
+            using(var scope=host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    WebStoreContext context = services.GetRequiredService<WebStoreContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
+                }
+            }
+            
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
