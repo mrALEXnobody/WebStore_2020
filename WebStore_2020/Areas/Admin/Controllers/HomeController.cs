6a5ca16687a56e805523fd910d6c33db666c7f03
Александр Гусев <scorp.predateur@gmail.com>
2020-05-11 17:28:18 +0300

    Занятие 8: область админки (admin area)

---
+++ WebStore_2020/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DomainNew;
+using WebStore_2020.Infrastructure.Interfaces;
+
+namespace WebStore_2020.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admins")]
+    public class HomeController : Controller
+    {
+        private readonly IProductService productService;
+
+        public HomeController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            var products = productService.GetProducts(new ProductFilter());
+
+            return View(products);
+        }
+    }
+}
--- WebStore_2020/Startup.cs
+++ WebStore_2020/Startup.cs
@@ -121,6 +121,10 @@ namespace WebStore_2020
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
